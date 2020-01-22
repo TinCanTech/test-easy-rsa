2d16b48a7e9c96685ee23f53785351e3eb3de302
Richard Bonhomme <tincanteksup@gmail.com>
2020-01-22 14:51:02 +0000

    export ERSA_UTEST_CURL_TARGET
    Signed-off-by: Richard Bonhomme <tincanteksup@gmail.com>

--- op_test.sh
+++ op_test.sh
@@ -6,9 +6,9 @@
 
 # Set in Travis ERSA_UTEST_CURL_TARGET="tincantech/easyrsa-unit-tests"
 
-#ERSA_UTEST_CURL_TARGET="${ERSA_UTEST_CURL_TARGET:-OpenVPN/easyrsa-unit-tests}"
-ERSA_UTEST_CURL_TARGET="${ERSA_UTEST_CURL_TARGET:-tincantech/easyrsa-unit-tests}"
-#ERSA_UTEST_CURL_TARGET="${ERSA_UTEST_CURL_TARGET:-tincantech/test-easyrsa-unit-tests}"
+#export ERSA_UTEST_CURL_TARGET="${ERSA_UTEST_CURL_TARGET:-OpenVPN/easyrsa-unit-tests}"
+export ERSA_UTEST_CURL_TARGET="${ERSA_UTEST_CURL_TARGET:-tincantech/easyrsa-unit-tests}"
+#export ERSA_UTEST_CURL_TARGET="${ERSA_UTEST_CURL_TARGET:-tincantech/test-easyrsa-unit-tests}"
 
 curl -O "https://raw.githubusercontent.com/$ERSA_UTEST_CURL_TARGET/master/easyrsa-unit-tests.sh"
 EXIT_CODE=1
