742a75e1e26f8a27727125cf91734e73636679f2
Richard Bonhomme <tincanteksup@gmail.com>
2020-01-22 13:00:17 +0000

    Max output
    Signed-off-by: Richard Bonhomme <tincanteksup@gmail.com>

--- op_test.sh
+++ op_test.sh
@@ -4,14 +4,17 @@
 # and executes that - allows for disconnected testing from the easy-rsa
 # repo with TravisCI.
 
-#CURL_TARGET="OpenVPN/easyrsa-unit-tests"
-
+ERSA_UTEST_CURL_TARGET="OpenVPN/easyrsa-unit-tests"
 export ERSA_UTEST_CURL_TARGET="${ERSA_UTEST_CURL_TARGET:-tincantech/test-easyrsa-unit-tests}"
+
 curl -O "https://raw.githubusercontent.com/$ERSA_UTEST_CURL_TARGET/master/easyrsa-unit-tests.sh"
+EXIT_CODE=1
 
 if [ -e "easyrsa-unit-tests.sh" ];
 then
-	sh easyrsa-unit-tests.sh -vv
+	EXIT_CODE=0
+	sh easyrsa-unit-tests.sh -vv || EXIT_CODE=1
+	rm easyrsa-unit-tests.sh
 fi
 
-# rm easyrsa-unit-tests.sh
+exit "$EXIT_CODE"
